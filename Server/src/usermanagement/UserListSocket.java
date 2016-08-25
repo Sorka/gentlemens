07195d39b38676167330902ad92a8aff7bc0925b
Sorka <linkmail@gmx.de>
2016-08-25 15:48:25 +0200

    Socket (leer)

---
+++ Server/src/usermanagement/UserListSocket.java
@@ -0,0 +1,11 @@
+package usermanagement;
+
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * Created by jonas on 25.08.2016.
+ */
+@ServerEndpoint( value="/userlist")
+public class UserListSocket {
+
+}
